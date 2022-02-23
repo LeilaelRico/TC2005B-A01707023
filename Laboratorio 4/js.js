7934f1dd31a63e2d3866ac1d069b1568a72a4652
LeilaelRico <A01707023@tec.mx>
2022-02-22 21:55:47 -0600

    Se añade ejercicio 4.

--- Laboratorio 4/js.js
+++ Laboratorio 4/js.js
@@ -9,7 +9,7 @@ const ej1 = prompt ("Ingresa un n\u00Famero");
 console.log(ej1);
 
 for (let i = 1; i <= ej1; i++) {
-    document.write(i + " ");
+    document.write("Ejercicio 1: " + i + " ");
 }
 
 /* Ejercicio 2 */
@@ -27,15 +27,13 @@ if (res1 == ej2) {
 
     let tiempo = (finTiempo - inTiempo) / 1000;
     alert("El resultado es correcto, tardaste " + tiempo + " segundos.");
-    document.write("<br>La respuesta de sumar " + rd1 + " + " + rd2 + " es " + res1);
-    document.write("<br>Su respuesta fue " + ej2 + " y tard\u00f3 " + tiempo + " segundos.");
+    document.write("<br>La respuesta de sumar " + rd1 + " + " + rd2 + " es " + res1+ " -->  Su respuesta fue " + ej2 + " y tard\u00f3 " + tiempo + " segundos.");
 
 } else {
 
     let tiempo = (finTiempo - inTiempo) / 1000;
     alert("El resultado es incorrecto, tardaste " + tiempo + " segundos.");
-    document.write("<br>La respuesta de sumar " + rd1 + " + " + rd2 + " es " + res1);
-    document.write("<br>Su respuesta fue " + ej2 + " y tard\u00f3 " + tiempo + " segundos.");
+    document.write("<br>Ejercicio 2 -> La respuesta de sumar " + rd1 + " + " + rd2 + " es " + res1 + " -->  Su respuesta fue " + ej2 + " y tard\u00f3 " + tiempo + " segundos.");
 
 }
 
@@ -69,7 +67,7 @@ function contador(array) {
         }
     }
 
-    let ej3 = "<br>Ceros: " + ceros + "<br>Negativos: " + negativos + "<br>Enteros: " + enteros;
+    let ej3 = "<br>Ejercicio 3: Ceros: " + ceros + " -> Negativos: " + negativos + " -> Enteros: " + enteros;
     return ej3;
 }
 
@@ -77,3 +75,26 @@ document.write(contador(arr));
 
 /* Ejercicio 4 */
 
+var mt = [[15,20,35,45],[45,74,2,33],[7,41,89,12],[98,0,61,4]];
+
+function promedios(matriz){
+    
+    let arrProm = new Array;
+    let promedio = 0;
+
+    for (let i = 0; i < matriz.length; i++) {
+
+        let sumPromedio = 0;
+        
+        for (let j = 0; j < matriz[i].length; j++) {
+
+            sumPromedio += matriz[i][j];  
+        }
+        promedio = sumPromedio / 4;
+        arrProm.push((" " + promedio));
+    }
+    let ej4 = "<br>Ejercicio 4: " + arrProm;
+    return ej4;    
+}
+
+document.write(promedios(mt));
