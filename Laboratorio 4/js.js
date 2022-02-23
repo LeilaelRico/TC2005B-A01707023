ff4c79c834e4e0f1707bcc2ed90441d03dda323e
LeilaelRico <A01707023@tec.mx>
2022-02-22 23:26:55 -0600

    Se añade ejercicio 6.

--- Laboratorio 4/js.js
+++ Laboratorio 4/js.js
@@ -6,10 +6,9 @@
 /* Ejercicio 1 */
 
 const ej1 = prompt ("Ingresa un n\u00Famero");
-console.log(ej1);
 
 for (let i = 1; i <= ej1; i++) {
-    document.write("Ejercicio 1: " + i + " ");
+    document.write(i + " ");
 }
 
 /* Ejercicio 2 */
@@ -98,3 +97,56 @@ function promedios(matriz){
 }
 
 document.write(promedios(mt));
+
+/* Ejercicio 5 */
+
+const ej5 = prompt ("Ingresa un n\u00Famero para invertir");
+console.log(ej5);
+
+var inverso = ej5.split('').reverse().join('');
+
+document.write("<br>Ejercicio 5: " + inverso);
+
+/* Ejercicio 6 */
+
+class Tiempo {
+    constructor (horas, minutos, segundos) {
+
+        this.horas = horas;
+        this.minutos = minutos;
+        this.segundos = segundos;
+    
+      }
+}
+
+
+const sec = prompt ("Ingrese el tiempo en segundos");
+
+function convierteSegundos(seg) {
+
+    this.horas = Math.floor(seg / 3600);
+    let residuo = seg % 3600;
+    this.minutos = Math.floor(residuo / 60);
+    this.segundos = residuo % 60;
+
+    let ej61 = document.write("<br> Ejercicio 6.1 = Las horas son: " + this.horas + " los minutos son: " + this.minutos + " los segundos son: " + this.segundos);
+    return ej61;
+
+}
+convierteSegundos(sec);
+
+const mn = prompt ("Ingrese el tiempo en minutos");
+
+function convierteMinutos(min) {
+
+    this.horas = Math.floor(min / 60);
+    let residuo = min % 60;
+    this.minutos = residuo;
+    this.segundos = residuo / 60;
+
+    let ej62 = document.write("<br> Ejercicio 6.2 = Las horas son: " + this.horas + " los minutos son: " + residuo + " los segundos son: " + this.segundos);
+    return ej62;
+
+}
+
+convierteMinutos(mn);
