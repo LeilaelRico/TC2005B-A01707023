fb5f0c9f7b815ea3988888a1425ea327f12e8cd2
LeilaelRico <A01707023@tec.mx>
2022-03-24 01:33:00 -0600

    Se termina laboratorio 12.

--- Laboratorio12/app.js
+++ Laboratorio12/app.js
@@ -16,27 +16,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 const miRuta = require('./routes/lab12.routes');
 app.use('/', miRuta);
 
+const exp = require('./routes/explicacion.routes');
+app.use('/expl', exp);
 
-/*//Middleware
-app.use((request, response, next) => {
-    console.log('Middleware!');
-    next(); //Le permite a la petición avanzar hacia el siguiente middleware
-});
-
-app.use('/ruta', (request, response, next) => {
-    console.log('Respuesta de la ruta "/ruta"');
-    response.send('Respuesta de la ruta "/ruta"'); 
-});
-
-app.use('/hola', (request, response, next) => {
-    console.log('Respuesta de la ruta "/hola"');
-    response.send('¡Hola! Esta es la respuesta de la ruta "/hola"'); 
-});
-
-
-app.use((request, response, next) => {
-    console.log('Otro middleware!');
-    response.send('¡Hola mundo!'); //Manda la respuesta
-});
-*/
 app.listen(3000);
--- Laboratorio12/routes/explicacion.routes.js
+++ Laboratorio12/routes/explicacion.routes.js
@@ -11,8 +11,7 @@ const router = express.Router();
 router.get('/', (request, response, next) => {
 
     console.log(request.body);
-    let respuesta = '<!DOCTYPE html><html lang="es-mx"><head><meta charset="utf-8"><title>A01707023_Lab11</title><link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"></head> <body> <div class="text-center"><h1 class="display-4 fst-italic">Cristian Leilael Rico Espinosa</h1></div>`) <div class="row d-flex justify-content-center"><div class="col-md-6"><article class="blog-post"><h2 class="blog-post-title fst-italic">Package.json</h2> <p>En base a <a href="https://nodejs.org/en/knowledge/getting-started/npm/what-is-the-file-package-json/">Node.js</a>, se tratan de archivos ubicados usualmente en la raíz de un proyecto los cuales le dan iformación al NPM para que pueda identificar al mismo así como a todas sus dependencias. De igual manera, puede contener "metadata" como una descripción del proyecto, la versión de la distribución o inclusive el tipo de licencia que el proyecto de Node.js posee.</p><hr></div></div> </body></html>';
-    response.send(respuesta);
+    response.render('pregunta.ejs');
 });
 
 
